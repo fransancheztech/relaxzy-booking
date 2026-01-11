bae197d62a8bf83d5b11e0d6b53110c3d8bc6384
Fran Sanchez <fransancheztech@gmail.com>
2026-01-11 18:48:58 +0100

    feat: :sparkles: Added New Booking button to bookings page

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,7 +1,34 @@
-const BookingsPage = () => {
-  return (
-    <div>BookingsPage</div>
-  )
-}
+'use client'
 
-export default BookingsPage
+import { useEffect, useState } from "react";
+import { useLayout } from "../context/LayoutContext";
+import NewBookingDialogForm from "@/components/Dialogs/NewBooking/DialogForm";
+
+const BookingsPage = () => {
+  const { setButtonLabel, setOnButtonClick } = useLayout();
+  const [isOpenDialogFormNewBooking, setIsOpenDialogFormNewBooking] =
+    useState(false);
+
+  useEffect(() => {
+    setButtonLabel("New Booking");
+    setOnButtonClick(
+      () => () => setIsOpenDialogFormNewBooking((prev) => !prev)
+    );
+    return () => {
+      setButtonLabel("");
+      setOnButtonClick(null);
+    };
+  }, [setButtonLabel, setOnButtonClick, setIsOpenDialogFormNewBooking]);
+
+  return (
+    <main>
+      <div>BookingsPage</div>
+      <NewBookingDialogForm
+        open={isOpenDialogFormNewBooking}
+        onClose={() => setIsOpenDialogFormNewBooking(false)}
+      />
+    </main>
+  );
+};
+
+export default BookingsPage;
